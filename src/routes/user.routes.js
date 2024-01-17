1652a51b9c35c8026b3d4f7cfb521bf968bb1845
Christian <filetelopezchristian@gmail.com>
2024-01-17 09:09:43 -0600

    added validation token :monkey:

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express'
+import { validateToken } from '../middlewares/validateToken'
 import * as userController from '../controllers/user.controller'
 
 const router = Router()
 
-router.post('/', userController.createUser)
+router.post('/', validateToken, userController.createUser)
 
 router.get('/', userController.getUsers)
 
 router.get('/:id', userController.getUser)
 
-router.put('/:id', userController.updateUser)
+router.put('/:id', validateToken, userController.updateUser)
 
-router.delete('/:id', userController.deleteUser)
+router.delete('/:id', validateToken, userController.deleteUser)
 
 export default router
