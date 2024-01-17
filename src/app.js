179197cd2e72c6efffc9c26b311fca9785345ccb
Christian <filetelopezchristian@gmail.com>
2024-01-17 15:03:03 -0600

    Finished file :boom: :100:

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import 'dotenv/config'
 import cors from 'cors'
+import path from 'path'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 import bookRouter from './routes/book.routes'
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
+app.use('/images', express.static(path.resolve(__dirname, '../public/images')))
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/books', bookRouter)
