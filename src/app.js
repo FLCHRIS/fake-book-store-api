f91386a0085b9857bd3c9122cbc304b1e9088fbc
Christian <filetelopezchristian@gmail.com>
2024-01-17 08:59:54 -0600

    Book modules added :ghost:

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cors from 'cors'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+import bookRouter from './routes/book.routes'
 
 const app = express()
 
@@ -13,5 +14,6 @@ app.use(express.json())
 
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/books', bookRouter)
 
 export default app
