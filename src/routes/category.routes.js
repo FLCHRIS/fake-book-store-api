b880652eb6e653243b9f1b6f6a873733cebb87a6
Christian <filetelopezchristian@gmail.com>
2024-01-17 14:26:39 -0600

    Finished file :boom: :100:

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import { validateToken } from '../middlewares/validateToken'
 import * as categoryController from '../controllers/category.controller'
 
 const router = Router()
 
-router.post('/', validateToken, categoryController.createCategory)
 router.get('/', categoryController.getCategories)
 router.get('/:id/books', categoryController.getBooksCategories)
 
