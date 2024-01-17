a8fe9e021bfe4221998cd2671305ce31e11c086c
Christian <filetelopezchristian@gmail.com>
2024-01-17 10:24:14 -0600

    Finished file :boom: :100:

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -18,8 +18,10 @@ export const createBook = async (req, res) => {
 }
 
 export const getBooks = async (req, res) => {
+  const { limit } = req.query || { limit: 0 }
+
   try {
-    const books = await Book.find()
+    const books = await Book.find().limit(limit)
 
     return res.json(books)
   } catch (error) {
@@ -46,9 +48,40 @@ export const getBook = async (req, res) => {
 }
 
 export const updateBook = async (req, res) => {
+  const id = req.params.id
+  const book = req.body
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'Invalid parameter.', message: 'Please submit a valid ID.' })
+  }
+
+  if (Object.keys(book).length === 0) {
+    return res.status(400).json({ error: 'Invalid parameter.', message: 'Please submit a book.' })
+  }
+
+  const updatedBook = {
+    _id: id,
+    name: book.name,
+    description: book.description,
+    number_of_pages: book.number_of_pages,
+    language: book.language,
+    publication_date: book.publication_date,
+    dimensions: book.dimensions,
+    image: book.image,
+    price: book.price,
+    author: book.author,
+    category: book.category
+  }
+
+  return res.json(updatedBook)
 }
 
 export const deleteBook = async (req, res) => {
+  const id = req.params.id
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'Invalid parameter.', message: 'Please submit a valid ID.' })
+  }
+
+  return res.status(204).end()
 }
