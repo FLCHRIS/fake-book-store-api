7ec0a9462c0bd9e3fb71cb4f9599621a073db568
Christian <filetelopezchristian@gmail.com>
2024-01-17 18:45:24 -0600

    Finished file :boom: :100:

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/fakebookstoredb')
+mongoose.connect(process.env.DATABASE_URL)
   .then(db => console.log('Database is connected'))
   .catch(error => console.log(error))
