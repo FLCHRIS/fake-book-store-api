55bf07f815c56eeec47f1d3fe1fa35b4ef312481
Christian <filetelopezchristian@gmail.com>
2024-01-27 18:28:44 -0600

    Non-important changes :monkey:

--- build/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_mongoose["default"].connect(process.env.DATABASE_URL).then(function (db) {
-  return console.log('Database is connected');
-})["catch"](function (error) {
-  return console.log(error);
-});
--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = process.env.PORT;
-_app["default"].listen(PORT, function () {
-  console.log("Server listen on port ".concat(PORT));
-});
