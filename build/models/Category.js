c7e259a350f318a95d11b9c1351bac14841dd303
Christian <filetelopezchristian@gmail.com>
2024-01-28 11:09:20 -0600

    Non-important changes :monkey:

---
+++ build/models/Category.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _mongoose = require("mongoose");
+var categorySchema = new _mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+}, {
+  versionKey: false
+});
+var _default = exports["default"] = (0, _mongoose.model)('Category', categorySchema);
